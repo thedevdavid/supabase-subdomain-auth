0cac44d0f30791ef7b6e10cd2d4b609147c705ec
David Levai <david@davidlevai.com>
2024-07-07 13:39:12 -0400

    removed unnecessary rewrites, added auth redirects

--- apps/dash/next.config.mjs
+++ apps/dash/next.config.mjs
@@ -12,14 +12,36 @@ const nextConfig = {
       destination: "/",
       permanent: false,
     },
+    {
+      source: "/dashboard/:path*",
+      missing: [
+        {
+          type: "cookie",
+          key: "supabase-subdomain-auth",
+        },
+      ],
+      destination: "/auth/signin",
+      permanent: false,
+    },
+    {
+      permanent: true,
+      source: "/auth",
+      destination: "/auth/signin",
+    },
+    {
+      source: "/login",
+      destination: "/auth/signin",
+      permanent: true,
+    },
+    {
+      source: "/signin",
+      destination: "/auth/signin",
+      permanent: true,
+    },
   ],
   rewrites: async () => {
     return {
       beforeFiles: [
-        {
-          source: "/auth",
-          destination: "/auth/login",
-        },
         {
           source:
             "/:path((?!auth|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
